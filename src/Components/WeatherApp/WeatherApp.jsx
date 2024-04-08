3f684aa4fb1bd8f26e2ae7065f3ebc8849a62a39
Jason Ling <Jasonling1996@gmail.com>
2024-04-08 16:41:23 -0700

    add more style and components to weather app

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './WeatherApp.css'
 import searchIcon from '../Assets/search.png'
 import clearIcon from '../Assets/clear.png'
+import cloudIcon from '../Assets/cloud.png'
 import drizzleIcon from '../Assets/drizzle.png'
 import humidIcon from '../Assets/humidity.png'
 import rainIcon from '../Assets/rain.png'
@@ -18,6 +19,39 @@ export const WeatherApp = () => {
           placeholder='search'/>
         <div className='search-icon'>
           <img src={searchIcon} alt=''/>
+        </div>
+      </div>
+      <div className='weather-image'>
+        <img src={cloudIcon} alt='' />
+      </div>
+      <div className="weather-temp">
+        24c
+      </div>
+      <div className="weather-location">
+        London
+      </div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidIcon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">
+              64%
+            </div>
+            <div className="text">
+              Humidity
+            </div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={windIcon} alt="" className="icon" />
+          <div className="data">
+            <div className="wind-speed">
+              18 km/h
+            </div>
+            <div className="text">
+              Wind Speed
+            </div>
+          </div>
         </div>
       </div>
     </div>
