3186517388d89e2213fb542a9b9e39a5914361d8
Jason Ling <Jasonling1996@gmail.com>
2024-04-08 16:01:51 -0700

    Clear template and imported icons

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './WeatherApp.css'
+import searchIcon from '../Assets/search.png'
+import clearIcon from '../Assets/clear.png'
+import drizzleIcon from '../Assets/drizzle.png'
+import humidIcon from '../Assets/humidity.png'
+import rainIcon from '../Assets/rain.png'
+import snowIcon from '../Assets/snow.png'
+import windIcon from '../Assets/wind.png'
+
+export const WeatherApp = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default WeatherApp
