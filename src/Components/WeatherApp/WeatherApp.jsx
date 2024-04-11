2c51d2f5d72c40931e6c6b55c867dbec9e13a654
Jason Ling <Jasonling1996@gmail.com>
2024-04-11 13:26:01 -0700

    change temp and add icons to change depending on api

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 import searchIcon from '../Assets/search.png'
-import clearIcon from '../Assets/clear.png'
-import cloudIcon from '../Assets/cloud.png'
-import drizzleIcon from '../Assets/drizzle.png'
-import humidIcon from '../Assets/humidity.png'
-import rainIcon from '../Assets/rain.png'
+import clearSkyDayIcon from '../Assets/clear_sky_day.png'
+import clearSkyNightIcon from '../Assets/clear_sky_night.png'
+import fewCloudsDayIcon from '../Assets/few_clouds_day.png'
+import fewCloudsNightIcon from '../Assets/few_clouds_night.png'
+import scatteredCloudIcon from '../Assets/scattered_clouds.png'
+import brokenCloudIcon from '../Assets/broken_clouds.png'
+import showerIcon from '../Assets/shower_rain.png'
+import rainDayIcon from '../Assets/rain_day.png'
+import rainNightIcon from '../Assets/rain_night.png'
+import thunderIcon from '../Assets/thunderstorm.png'
 import snowIcon from '../Assets/snow.png'
+import mistIcon from '../Assets/mist.png'
+import humidIcon from '../Assets/humidity.png'
 import windIcon from '../Assets/wind.png'
+
 let apiKey = process.env.REACT_APP_API_KEY;
 
 export const WeatherApp = () => {
+
+  const [icon, setIcon] = useState(scatteredCloudIcon);
 
   const search = async () => {
     const element = document.getElementsByClassName('city')
@@ -28,9 +38,57 @@ export const WeatherApp = () => {
     const location = document.getElementsByClassName('weather-location');
 
     humidity[0].innerHTML = data.main.humidity + "%";
-    temp[0].innerHTML = data.main.temp + "°c";
-    wind[0].innerHTML = data.wind.speed + " km/h";
+    temp[0].innerHTML = Math.floor(data.main.temp) + "°c";
+    wind[0].innerHTML = data.wind.speed + "km/h";
     location[0].innerHTML = data.name;
+
+    if(data.weather[0].icon === '01d') {
+      setIcon(clearSkyDayIcon);
+    }
+
+    if(data.weather[0].icon === '01n') {
+      setIcon(clearSkyNightIcon);
+    }
+
+    if(data.weather[0].icon === '02d') {
+      setIcon(fewCloudsDayIcon);
+    }
+
+    if(data.weather[0].icon === '02n') {
+      setIcon(fewCloudsNightIcon);
+    }
+
+    if(data.weather[0].icon === '03d' || data.weather[0].icon === '03n') {
+      setIcon(scatteredCloudIcon);
+    }
+
+    if(data.weather[0].icon === '04d' || data.weather[0].icon === '04n') {
+      setIcon(brokenCloudIcon);
+    }
+
+    if(data.weather[0].icon === '09d' || data.weather[0].icon === '09n') {
+      setIcon(showerIcon);
+    }
+
+    if(data.weather[0].icon === '10d') {
+      setIcon(rainDayIcon);
+    }
+
+    if(data.weather[0].icon === '10n') {
+      setIcon(rainNightIcon);
+    }
+
+    if(data.weather[0].icon === '11d' || data.weather[0].icon === '11n') {
+      setIcon(thunderIcon);
+    }
+
+    if(data.weather[0].icon === '13d' || data.weather[0].icon === '13n') {
+      setIcon(snowIcon);
+    }
+
+    if(data.weather[0].icon === '50d' || data.weather[0].icon === '50n') {
+      setIcon(mistIcon);
+    }
   }
 
   return (
@@ -49,10 +107,10 @@ export const WeatherApp = () => {
         </div>
       </div>
       <div className='weather-image'>
-        <img src={cloudIcon} alt='' />
+        <img src={icon} alt='' />
       </div>
       <div className="weather-temp">
-        24c
+        24°c
       </div>
       <div className="weather-location">
         London
